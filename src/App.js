6dacc2f0a6f0d2be61e393ad073b0039fd58a71e
Paylak Navasardyan <paylak2002n@gmail.com>
2025-01-28 18:33:38 +0400

    About Us is responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './footer/Footer';
 import Mainpage from './mainpage/Mainpage';
@@ -9,36 +9,36 @@ import Aboutus from './abutuspage/Aboutus';
 function App() {
   return (
     <div className="App">    
-      <Routes>
+      <Routes>       
         <Route path={'/'} element = {
           <>
             <Navbar />
             <Mainpage />
-            <Footer />
-          </>
-        } />
+               <Footer />
+            </>
+          } />
 
-        <Route path='/products' element = {
-          <>
-            <Navbar />
-            <Productspage />
-            <Footer />
-          </>
-        } />
-        
-        <Route path='/about' element = {
-          <>
-            <Navbar />
-            <Aboutus />
-            <Footer />
-          </>
-        } />
-        
-        <Route path='*' element = {
-          <>
-            <Navbar />
-            <div><h2>Page is not found</h2></div>
-          </>
+          <Route path='/products' element = {
+            <>
+              <Navbar />
+              <Productspage />
+              <Footer />
+            </>
+          } />
+          
+          <Route path='/about' element = {
+            <>
+              <Navbar />
+              <Aboutus />
+              <Footer />
+            </>
+          } />
+          
+          <Route path='*' element = {
+            <>
+              <Navbar />
+              <div><h2>Page is not found</h2></div>
+            </>
           } />
       </Routes>
     </div>    
