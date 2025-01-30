be9fd94856ee0aa5c527517f4901baf758ca38e5
Paylak Navasardyan <paylak2002n@gmail.com>
2025-01-30 14:49:21 +0400

    add state for registration

--- src/loginpage/Login.jsx
+++ src/loginpage/Login.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './Login.module.css';
 import image from '../exportImages/ExportImage';
 
 export default function Login() {
+    const [action, setAction] = useState('');
+
+    const addClass = () => {
+        setAction('active')
+    }
+
+    const deleteClass = () => {
+        setAction('')
+    }
+
   return (
-    <div className={classes.loginPage}>
+    <div className={`${classes.loginPage} ${action ? classes.active : ''}`}>
         <div className={classes.loginPageBorder}>
             <h2>Login</h2>
 
@@ -28,14 +38,47 @@ export default function Login() {
                 
                 <a href="#">Forgot password</a>
             </div>        
-
             
-            <button>Login</button>
-           
+            <button>Login</button>           
 
             <div className={classes.loginPageAccount}>
                 <span>Don't have an account?</span>
-                <a href="#">Register</a>
+                <a href="#" onClick={addClass}>Register</a>
+            </div>
+        </div>
+
+        <div className={classes.registerPageBorder}>
+            <h2>Register</h2>
+
+            <div className={classes.loginPageInputs}>  
+                <div className={classes.loginPageInputsInput}>  
+                    <input type="text" placeholder='Username' required />
+                    <img src={image.user} alt='icon'/>
+                </div>
+
+                <div className={classes.loginPageInputsInput}>  
+                    <input type="email" placeholder='Email' required />
+                    <img src={image.email} alt='icon'/>
+                </div>
+
+                <div className={classes.loginPageInputsInput}>  
+                    <input type="password"  placeholder='Password' required />
+                    <img src={image.lock} alt='icon'/>
+                </div>
+            </div>
+
+            <div className={classes.loginPageRememberPassword}>
+                <label>
+                    <input type='checkbox'></input>
+                    <span>I agree to the terms & conditions</span>
+                </label>
+            </div>        
+            
+            <button>Register</button>
+
+            <div className={classes.loginPageAccount}>
+                <span>Already have an account?</span>
+                <a href="#" onClick={deleteClass}>Login</a>
             </div>
         </div>
     </div>
